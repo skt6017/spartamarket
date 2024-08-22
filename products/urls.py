5b4e9c9cd0d06b9e27539887f8723741977835ad
kimhangyu <khk201@naver.com>
2024-08-22 23:20:18 +0900

    feat. 상품 수정
    - 기존 product_create.html 명 수정
    - urls.py path
    - views.py def
    - templates html

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from. import views
 app_name="products"
 urlpatterns = [
     path('create/', views.product_create, name='product_create'),
+    path('update/<int:pk>/', views.product_update, name='product_update'),
     ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProductForm
+from .models import Product
 
 # Create your views here.
 
@@ -19,4 +20,23 @@ def product_create(request):
 
     # 폼을 컨텍스트에 담아 템플릿으로 렌더링
     context = {"form": form}
-    return render(request, "products/products_create.html", context)
+    return render(request, "products/product_create.html", context)
+
+def product_update(request, pk):
+    # pk를 기반, Product 객체를 가져오고, 없으면 404
+    product = get_object_or_404(Product, pk=pk)
+    
+    # 요청이 POST인 경우, 
+    if request.method == 'POST':
+        # POST 데이터를 기반으로 기존 product 인스턴스를 업데이트하기 위해 폼을 초기화
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            # 유효성 검사를 통과하면 폼 데이터 저장
+            form.save()
+            # 상품 상세 페이지로 리다이렉트
+            return redirect('product_detail', pk=product.pk)
+    else:
+        form = ProductForm(instance=product)
+    
+    # 폼과 product 데이터를 컨텍스트에 담아 템플릿에 전달
+    return render(request, 'products/product_update.html', {'form': form, 'product': product})
